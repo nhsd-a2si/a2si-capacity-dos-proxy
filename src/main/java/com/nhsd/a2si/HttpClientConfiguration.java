acb6734be97c406c4fc38bc6f48815825e0ca39b
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-09-14 11:37:49 +0100

    Adds basic auth to DoS Proxy and configures it to use REST batch

--- src/main/java/com/nhsd/a2si/HttpClientConfiguration.java
+++ src/main/java/com/nhsd/a2si/HttpClientConfiguration.java
@@ -1,7 +1,11 @@
 package com.nhsd.a2si;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +34,7 @@ public class HttpClientConfiguration {
 	private int maxConnTotal;    // max concurrent connections
 	private int maxConnPerRoute; // max concurrent connections to one url
 
+
 	@Bean
     public RequestConfig dosRequestConfig(
         	@Value("${dos.httpClient.socketTimeout}") int socketTimeout,
@@ -47,13 +52,23 @@ public class HttpClientConfiguration {
                 .build();
     }
     
-    public HttpClientBuilder httpClientBuilder() {
+    public HttpClientBuilder httpClientBuilder(CredentialsProvider credentialsProvider) {
     	HttpClientBuilder builder = HttpClientBuilder.create();
     	builder.setDefaultRequestConfig(dosRequestConfig(socketTimeout, connectTimeout, connectionRequestTimeout));
     	builder.setMaxConnTotal(maxConnTotal);
     	builder.setMaxConnPerRoute(maxConnPerRoute);
+    	builder.setDefaultCredentialsProvider(credentialsProvider);
     	return builder;
     }
+
+
+	@Bean
+	public CredentialsProvider credentialsProvider(String capacityServiceUsername, String capacityServicePassword){
+		CredentialsProvider provider = new BasicCredentialsProvider();
+		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(capacityServiceUsername, capacityServicePassword);
+		provider.setCredentials(AuthScope.ANY, credentials);
+		return provider;
+	}
     
     @Bean
     public HttpClient dosHttpClient(
@@ -61,7 +76,9 @@ public class HttpClientConfiguration {
         	@Value("${dos.httpClient.maxConnPerRoute}") int maxConnPerRoute,
         	@Value("${dos.httpClient.socketTimeout}") int socketTimeout,
         	@Value("${dos.httpClient.connectTimeout}") int connectTimeout,
-        	@Value("${dos.httpClient.connectionRequestTimeout}") int connectionRequestTimeout
+        	@Value("${dos.httpClient.connectionRequestTimeout}") int connectionRequestTimeout,
+			@Value("${capacity.service.username}") String capacityServiceUsername,
+			@Value("${capacity.service.password}") String capacityServicePassword
     		) {
     	this.maxConnTotal = maxConnTotal;
     	this.maxConnPerRoute = maxConnPerRoute;
@@ -75,7 +92,7 @@ public class HttpClientConfiguration {
     	LOGGER.info("DoS connectTimeout=" + connectTimeout);
     	LOGGER.info("DoS connectionRequestTimeout=" + connectionRequestTimeout);
     	
-    	return httpClientBuilder().build();
+    	return httpClientBuilder(credentialsProvider(capacityServiceUsername, capacityServicePassword)).build();
     }
 
 }
