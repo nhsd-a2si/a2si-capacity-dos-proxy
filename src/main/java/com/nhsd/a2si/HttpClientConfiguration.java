e5fca1546877b329a8791d99bbb55fe33d289294
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-10-03 17:22:30 +0100

    CD-687 Fix for DoS Proxy reusing the HTTP Client with both Dos and the Capacity Service
    This resulted in Capacity Service's basic auth details being sent to DoS.

--- src/main/java/com/nhsd/a2si/HttpClientConfiguration.java
+++ src/main/java/com/nhsd/a2si/HttpClientConfiguration.java
@@ -3,17 +3,20 @@ package com.nhsd.a2si;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /*
  * This class constructs 2 beans, "dosRequestConfig" and "dosHttpClient" with settings for 
@@ -53,32 +56,22 @@ public class HttpClientConfiguration {
                 .setConnectionRequestTimeout(connectionRequestTimeout)
                 .build();
     }
-    
-    public HttpClientBuilder httpClientBuilder(CredentialsProvider credentialsProvider) {
-    	HttpClientBuilder builder = HttpClientBuilder.create();
-    	builder.setDefaultRequestConfig(dosRequestConfig(socketTimeout, connectTimeout, connectionRequestTimeout));
-    	builder.setMaxConnTotal(maxConnTotal);
-    	builder.setMaxConnPerRoute(maxConnPerRoute);
-    	builder.setDefaultCredentialsProvider(credentialsProvider);
-    	return builder;
-    }
 
-	public CredentialsProvider credentialsProvider(String capacityServiceUsername, String capacityServicePassword){
-		CredentialsProvider provider = new BasicCredentialsProvider();
-		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(capacityServiceUsername, capacityServicePassword);
-		provider.setCredentials(AuthScope.ANY, credentials);
-		return provider;
+	public HttpClientBuilder httpClientBuilder() {
+		HttpClientBuilder builder = HttpClientBuilder.create();
+		builder.setDefaultRequestConfig(dosRequestConfig(socketTimeout, connectTimeout, connectionRequestTimeout));
+		builder.setMaxConnTotal(maxConnTotal);
+		builder.setMaxConnPerRoute(maxConnPerRoute);
+		return builder;
 	}
-    
-    @Primary @Bean
+
+    @Bean
 	public CloseableHttpClient dosHttpClient(
         	@Value("${dos.httpClient.maxConnTotal}") int maxConnTotal,
         	@Value("${dos.httpClient.maxConnPerRoute}") int maxConnPerRoute,
         	@Value("${dos.httpClient.socketTimeout}") int socketTimeout,
         	@Value("${dos.httpClient.connectTimeout}") int connectTimeout,
-        	@Value("${dos.httpClient.connectionRequestTimeout}") int connectionRequestTimeout,
-			@Value("${capacity.service.username}") String capacityServiceUsername,
-			@Value("${capacity.service.password}") String capacityServicePassword
+        	@Value("${dos.httpClient.connectionRequestTimeout}") int connectionRequestTimeout
     		) {
     	this.maxConnTotal = maxConnTotal;
     	this.maxConnPerRoute = maxConnPerRoute;
@@ -92,7 +85,26 @@ public class HttpClientConfiguration {
     	LOGGER.info("DoS connectTimeout=" + connectTimeout);
     	LOGGER.info("DoS connectionRequestTimeout=" + connectionRequestTimeout);
     	
-    	return httpClientBuilder(credentialsProvider(capacityServiceUsername, capacityServicePassword)).build();
+    	return httpClientBuilder().build();
     }
+
+
+	@Bean
+	@Qualifier("capacityServiceClientRestTemplate")
+	public RestTemplate capacityServiceClientRestTemplate(@Value("${capacity.service.username}") String capacityServiceUsername,
+									 @Value("${capacity.service.password}") String capacityServicePassword) {
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setHttpClient(HttpClientBuilder.create().setDefaultCredentialsProvider(credentialsProvider(capacityServiceUsername, capacityServicePassword)).build());
+		return new RestTemplate(requestFactory);
+	}
+
+	public CredentialsProvider credentialsProvider(String capacityServiceUsername, String capacityServicePassword){
+		CredentialsProvider provider = new BasicCredentialsProvider();
+		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(capacityServiceUsername, capacityServicePassword);
+		provider.setCredentials(AuthScope.ANY, credentials);
+		return provider;
+	}
+
+
 
 }
